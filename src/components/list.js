8dec2f8f1b91718915bbccf81fb5c03fa1a99a22
Jeff Lieu <jefflieu@gmail.com>
2018-07-05 18:13:03 -0700

    updated redux todos with adding entries to DOM

--- src/components/list.js
+++ src/components/list.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {getData} from '../actions';
+import {Link} from 'react-router-dom';
 
 
 class List extends Component {
@@ -16,6 +17,9 @@ class List extends Component {
         return(
             <div>
                 <h1 className="center">To Do list now with Redux</h1>
+                <div className="row right-align">
+                    <Link className="btn red" to="/add-item">Add Item</Link>
+                </div>
                 <ul className="collection">
                     {listElements}
                 </ul>
