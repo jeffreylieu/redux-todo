38ca87c669dc01d5afc141242c872632c7105552
Jeff Lieu <jefflieu@gmail.com>
2018-07-09 14:38:09 -0700

    added error handing for single item page

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import List from './list';
-import {Route} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import AddItem from './add_item';
 import ViewItem from './view_item';
+import NotFound from './not_found';
+
 
 const App = () => (
     <div className="container">
-        <Route exact path = "/" component ={List}/>
-        <Route path = "/add-item" component ={AddItem}/>
-        <Route path = "/todo/:item_id" component={ViewItem}/>
+        <Switch>
+            <Route exact path = "/" component ={List}/>
+            <Route path = "/add-item" component ={AddItem}/>
+            <Route path = "/todo/:item_id" component={ViewItem}/>
+            <Route component ={NotFound}/>
+        </Switch>
     </div>
 );
 
